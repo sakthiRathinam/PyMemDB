0a6bde46807d07c9d4938f1610abec5f225e96de
sakthi <sakthiratnam050@gmail.com>
2024-02-23 06:53:37 +0530

    feat: Added ruff linter and vscode setup for formatting and linting

--- tests/test_simple_string_protocol.py
+++ tests/test_simple_string_protocol.py
@@ -4,5 +4,5 @@ from pymemdb.pymemdbprotocols.protocol_types import SimpleString
 
 def test_simple_string_parser():
     actual_output = simple_string_parser(b"+ok\r\n")
-    expected_frame = (SimpleString("ok"),2)
+    expected_frame = (SimpleString("ok"), 2)
     assert actual_output == expected_frame
