6e9bd94875ee4c6d9dda818414c2fdad24c44ae1
sakthi <sakthiratnam050@gmail.com>
2024-09-08 09:49:48 +0530

    feat: added test cases and background thread for run lazy expiry algo

--- pymemdb/pymemdbdatastructures/datastore.py
+++ pymemdb/pymemdbdatastructures/datastore.py
@@ -1,7 +1,7 @@
 import dataclasses as dc
+import random
 import threading
 import time
-from random import random
 from typing import Any
 
 
@@ -25,13 +25,14 @@ class DataStore:
     def __init__(self):
         self._data: dict = {}
         self._lock: threading.Lock = threading.Lock()
+        self._clean_expired_keys_thread_active = True
 
     def __getitem__(self, key: Any) -> Any:
         with self._lock:
             key_value = self._data.get(key, None)
             if not key_value:
                 return None
-            elif key_value is not None and key_value.ttl < time.time_ns():
+            elif key_value.ttl != 0 and key_value.ttl < time.time_ns():
                 del self._data[key]
                 return None
             return key_value.data
@@ -46,12 +47,36 @@ class DataStore:
         with self._lock:
             if format.lower() in ["ex", "px"]:
                 expiry_in_ns: int = time.time_ns() + to_ns(format, expiry)
-            expiry_in_ns = to_ns(format, expiry)
+            else:
+                expiry_in_ns = to_ns(format, expiry)
             self._data[key] = DataEntry(value, expiry_in_ns)
 
-    def lazy_expire(self):
-        random_keys = random.sample(self._data.keys(), 20)
+    def lazy_expire(self, lazy_expire_retry_percentage: int = 25) -> None:
+        total_no_of_keys = 0
+        cleaning_loop_is_active = True
         with self._lock:
-            for key in random_keys:
-                if self._data[key].ttl < time.time_ns():
-                    del self._data[key]
+            while cleaning_loop_is_active:
+                deleted_keys = 0
+                total_no_of_keys = len(self._data)
+                random_keys = random.sample(
+                    list(self._data.keys()), min(20, total_no_of_keys)
+                )
+                for key in random_keys:
+                    if (
+                        self._data[key].ttl != 0
+                        and self._data[key].ttl < time.time_ns()
+                    ):
+                        del self._data[key]
+                        deleted_keys += 1
+                if (
+                    total_no_of_keys * lazy_expire_retry_percentage
+                ) // 100 > deleted_keys or total_no_of_keys == 0:
+                    cleaning_loop_is_active = False
+
+    def stop_cleaning_expired_keys_thread(self) -> None:
+        self._clean_expired_keys_thread_active = False
+
+    def clean_expired_keys(self) -> None:
+        while self._clean_expired_keys_thread_active:
+            self.lazy_expire()
+            time.sleep(10)
--- tests/test_expiry_commands.py
+++ tests/test_expiry_commands.py
@@ -1,4 +1,5 @@
 import datetime
+import uuid
 
 import pytest
 from freezegun import freeze_time
@@ -60,10 +61,21 @@ def test_expiry_command_set_happy_flow(
             ),
             SimpleString("OK"),
             None,
-        )
+        ),
+        (
+            Array(
+                [
+                    BulkString(b"set"),
+                    BulkString(b"whoami"),
+                    BulkString(b"database-lover"),
+                ]
+            ),
+            SimpleString("OK"),
+            "database-lover",
+        ),
     ],
 )
-def test_lazy_expiry_algo(
+def test_lazy_expiry_algo_while_getting(
     command: Array,
     expected_output: SimpleString | SimpleError,
     expected_output_data: str | None,
@@ -75,3 +87,35 @@ def test_lazy_expiry_algo(
         time.tick(delta=datetime.timedelta(seconds=11))
         assert actual_output == expected_output
         assert datastore[str(command.data[1])] == expected_output_data
+
+
+def create_dummy_keys_in_datastore(
+    datastore: DataStore, no_of_keys: int, expiry_in_seconds: int
+) -> None:
+    if expiry_in_seconds == 0:
+        for i in range(no_of_keys):
+            datastore[f"key{i}-{uuid.uuid1()}"] = f"value{i}"
+    for i in range(no_of_keys):
+        datastore.set_item_with_expiry(
+            f"key{i}-{uuid.uuid1()}", f"value{i}", expiry_in_seconds, "ex"
+        )
+
+
+def test_background_lazy_expiry():
+    test_datastore = DataStore()
+
+    initial_datetime = datetime.datetime.now()
+    with freeze_time(initial_datetime) as time:
+        create_dummy_keys_in_datastore(test_datastore, 10, 10)
+        create_dummy_keys_in_datastore(test_datastore, 10, 30)
+        assert len(test_datastore._data) == 20
+
+        test_datastore.clean_expired_keys()
+        time.tick(delta=datetime.timedelta(seconds=10))
+        assert len(test_datastore._data.keys()) == 10
+
+        time.tick(delta=datetime.timedelta(seconds=10))
+        assert len(test_datastore._data.keys()) == 10
+
+        time.tick(delta=datetime.timedelta(seconds=10))
+        assert len(test_datastore._data.keys()) == 0
